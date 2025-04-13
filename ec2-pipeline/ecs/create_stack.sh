dbad41b67597f37612df9226376d09f896789d08
Mahesh Mahadeva <yelekeri@gmail.com>
2025-04-12 22:53:37 -0400

    ecs

--- ec2-pipeline/ecs/create_stack.sh
+++ ec2-pipeline/ecs/create_stack.sh
@@ -1,11 +1,7 @@
 SECONDS=0
-aws cloudformation create-stack --stack-name ecs-part1 --template-body file://ecs-part1.yaml  --capabilities CAPABILITY_NAMED_IAM $*
+aws cloudformation create-stack --stack-name ecs --template-body file://ecs.yaml  --capabilities CAPABILITY_NAMED_IAM $*
 
-aws cloudformation wait stack-create-complete --stack-name ecs-part1
-
-aws cloudformation create-stack --stack-name ecs-part2 --template-body file://ecs-part2.yaml  --capabilities CAPABILITY_NAMED_IAM $*
-
-aws cloudformation wait stack-create-complete --stack-name ecs-part2
+aws cloudformation wait stack-create-complete --stack-name ecs
 
 # To pass different environment information 
 # ./create_stack.sh --parameters ParameterKey=environment,ParameterValue=prod
--- ec2-pipeline/ecs/delete_stack.sh
+++ ec2-pipeline/ecs/delete_stack.sh
@@ -1,8 +1,3 @@
-aws cloudformation delete-stack --stack-name ecs-part2
+aws cloudformation delete-stack --stack-name ecs
 
-aws cloudformation wait stack-delete-complete --stack-name ecs-part2
-
-aws cloudformation delete-stack --stack-name ecs-part1
-
-aws cloudformation wait stack-delete-complete --stack-name ecs-part1
-
+aws cloudformation wait stack-delete-complete --stack-name ecs
