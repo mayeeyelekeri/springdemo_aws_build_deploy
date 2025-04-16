cc99e2fda66d6bbaadfcf8a75f6ff169f12fb8c0
Mahesh Mahadeva <yelekeri@gmail.com>
2025-04-16 08:44:34 -0400

    nothing

--- ec2-blue-green-codedeploy/ecs/delete_stack.sh
+++
@@ -1,3 +0,0 @@
-aws cloudformation delete-stack --stack-name ecs
-
-aws cloudformation wait stack-delete-complete --stack-name ecs
--- ec2-blue-green-codedeploy/ecs/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-# just call the script which takes care of creating parameters 
-. ~/INFO/secrets/ecs_params.sh  $1
- 
-./pushDockerImageToECR.sh
