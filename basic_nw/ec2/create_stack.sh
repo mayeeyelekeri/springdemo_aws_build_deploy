07ec0c0c2dee90b19d436f817ecf92c6503ec0bb
MM <mayeeyelekeri@gmail.com>
2025-04-29 13:47:45 +0000

    ec2 added image creation

--- basic_nw/ec2/create_stack.sh
+++ basic_nw/ec2/create_stack.sh
@@ -3,12 +3,17 @@ aws cloudformation create-stack --stack-name ec2 --template-body file://ec2.yaml
 
 aws cloudformation wait stack-create-complete --stack-name ec2
 
+# create AMI from the running ec2 instance 
+instanceId=`aws ec2 describe-instances --query "Reservations[0].Instances[0].InstanceId" --output text`
 
+`aws ec2 create-image --instance-id $instanceId --name "myDockerImage" --description "Docker Image with CodeDeploy agent"`
+
+# Wait till the AMI is created, usually it takes few minutes, wait in a loop 
 status=`aws ec2 describe-images --owners self --query "Images[0].State" --output text`
 
 while [ "$status" != "available" ] 
 do 
-   sleep 2 
+   sleep 5 
    status=`aws ec2 describe-images --owners self --query "Images[0].State" --output text`
    echo "status is $status" 
 done 
@@ -20,7 +25,7 @@ echo "AMI ID: $imageId"
 
 # Add ImageID to parameter store 
 aws ssm put-parameter \
-    --name "/dev/ec2/dockerImageID" \
+    --name "/dev/ec2/my_docker_image_id" \
     --type "String" \
     --value "$imageId" \
     --overwrite
